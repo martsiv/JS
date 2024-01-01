e6ed6c4b2f7cfaaf58c6bfd446e61c06ddc718fd
Mykola <mykola.martsiv@gmail.com>
2024-01-01 23:49:26 +0200

    Added l8

--- Lessons/Lesson8(Promises)/Task2/script.js
+++ Lessons/Lesson8(Promises)/Task2/script.js
@@ -1,17 +1,23 @@
-$("#loginInput").on('keypress', function (event) {
-    if (event.wich === 13) {
-        $login = $("#loginInput");
-        fetch(`https://api.github.com/users/${login.val}`)
-            .then(res => {
-                if (res.status === 404) {
-                    alert("Not found");
-                }
-                else {
-                    let fullResult = res.json();
-                    
-                }
-            })
-    }
+$(document).ready(function () {
+    $('#loginInput').keypress(function (event) {
+        if (event.which === 13) {
+            let inputValue = $(this).val();
+            searchInGit(inputValue);
+        }
+    });
 });
 
+function searchInGit(value) {
+    console.log('Handling entered value:', value);
+    // $login = $("#loginInput");
+    // fetch(`https://api.github.com/users/${login.val}`)
+    //     .then(res => {
+    //         if (res.status === 404) {
+    //             alert("Not found");
+    //         }
+    //         else {
+    //             let fullResult = res.json();
 
+    //         }
+    //     })
+}
