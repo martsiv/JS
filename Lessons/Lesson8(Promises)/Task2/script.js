47532f0248eacdb8fddc9217baa78816d001c1b7
Mykola <mykola.martsiv@gmail.com>
2024-01-02 02:32:37 +0200

    Completed task AJAX, Promises, Fetch, async/await

--- Lessons/Lesson8(Promises)/Task2/script.js
+++ Lessons/Lesson8(Promises)/Task2/script.js
@@ -1,23 +1,53 @@
-$(document).ready(function () {
-    $('#loginInput').keypress(function (event) {
-        if (event.which === 13) {
-            let inputValue = $(this).val();
-            searchInGit(inputValue);
-        }
-    });
+// Обробник події unhandledrejection (для браузерів і аналог для іншого оточення), 
+// щоб відстежувати необроблені помилки і інформувати про них користувача
+window.addEventListener('unhandledrejection', function (event) {
+    alert(event.promise); // [object Promise] - проміс, який згенерував помилку
+    alert(event.reason); // Error: Whoops! - об’єкт помилки, яка не була оброблена
 });
 
-function searchInGit(value) {
-    console.log('Handling entered value:', value);
-    // $login = $("#loginInput");
-    // fetch(`https://api.github.com/users/${login.val}`)
-    //     .then(res => {
-    //         if (res.status === 404) {
-    //             alert("Not found");
-    //         }
-    //         else {
-    //             let fullResult = res.json();
+let myForm = document.forms.searchingGitForm;
+let $photo = $('.photo img');
+let $name = $('.namePlaceholder');
+let $login = $('.loginPlaceholder');
+let $urlGit = $('.urlGitPlaceholder');
+let $blog = $('.urlBlog');
+let $country = $('.countryPlaceholder');
+let $emailPlaceholder = $('.emailPlaceholder');
+let $followersPlaceholder = $('.followersPlaceholder');
 
-    //         }
-    //     })
+myForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Зупиняємо стандартну поведінку форми (відправку на сервер)
+    handleEnterKeyPress();
+});
+
+function handleEnterKeyPress() {
+    let username = myForm.loginGitHub;
+    loadGithubUser(username.value)
+        .then(githubUser => setToDOM(githubUser))
+        .catch(error => alert("User not found. " + error.message));
+}
+
+function loadGithubUser(name) {
+    return loadJson(`https://api.github.com/users/${name}`);
+}
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`GitHub API request failed! Status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+function setToDOM(githubUser) {
+    $photo.attr("src", githubUser.avatar_url);
+    $name.append(githubUser.name);
+    $login.append(githubUser.login);
+    $urlGit.attr("href", githubUser.html_url);
+    $urlGit.text(githubUser.html_url);
+    $blog.attr("href", githubUser?.blog);
+    $blog.text(githubUser?.blog || "no blog");
+    $country.append(githubUser.location);
+    $emailPlaceholder.append(githubUser?.email ?? 'no email');
+    $followersPlaceholder.append(githubUser.followers);
 }
